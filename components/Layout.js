61b626278813e63791463ccd6bc561fde20a0ae0
WOLFPACK <officialdavid890@gmail.com>
2025-10-10 23:57:42 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Layout({ children }) {
   return (
     <div className="relative min-h-screen bg-black text-gray-100 overflow-hidden font-[Orbitron]">
-      {/* Watermark background */}
+      {/* Subtle Africa watermark */}
       <div className="absolute inset-0 opacity-[0.05] pointer-events-none">
         <Image
           src="/africa-map.png"
@@ -15,6 +15,7 @@ export default function Layout({ children }) {
         />
       </div>
 
+      {/* Main layout */}
       <div className="relative z-10 flex flex-col min-h-screen">
         <TopBar />
         <main className="flex-1 px-6 py-10">{children}</main>
