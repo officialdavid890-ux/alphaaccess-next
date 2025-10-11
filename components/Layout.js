59c2ca02d384ac969a58cf82a716175b9f944b87
WOLFPACK <officialdavid890@gmail.com>
2025-10-10 23:52:50 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,25 @@
-import TopBar from "@/components/TopBar";
-import BottomNav from "@/components/BottomNav";
+import TopBar from "./TopBar";
+import BottomNav from "./BottomNav";
+import Image from "next/image";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen bg-dark text-white">
-      <TopBar />
-      <main className="flex-1 overflow-y-auto p-4">{children}</main>
-      <BottomNav />
+    <div className="relative min-h-screen bg-black text-gray-100 overflow-hidden font-[Orbitron]">
+      {/* Watermark background */}
+      <div className="absolute inset-0 opacity-[0.05] pointer-events-none">
+        <Image
+          src="/africa-map.png"
+          alt="Africa Map"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <TopBar />
+        <main className="flex-1 px-6 py-10">{children}</main>
+        <BottomNav />
+      </div>
     </div>
   );
 }
