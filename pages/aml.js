b6adf3b47a2c305b30c189bd2b35ba320123773e
WOLFPACK <officialdavid890@gmail.com>
2025-10-11 12:53:33 -0700

    Update aml.js

--- pages/aml.js
+++ pages/aml.js
@@ -1,1 +1,5 @@
-
+// pages/aml.js
+import Layout from "../components/Layout";
+export default function Aml() {
+  return <Layout><h1 className="text-3xl font-bold text-gold-400">AML Policy</h1><p className="text-gold-300/80 mt-4">Our anti-money laundering compliance framework.</p></Layout>;
+}
