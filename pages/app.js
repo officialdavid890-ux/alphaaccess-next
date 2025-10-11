092667832f640a133e36ea3e663febbfb827b839
WOLFPACK <officialdavid890@gmail.com>
2025-10-10 21:45:48 -0700

    Create app.js

---
+++ pages/app.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+import HomeDashboard from "@/components/HomeDashboard";
+
+export default function AppShell() {
+  return (
+    <Layout>
+      <HomeDashboard />
+    </Layout>
+  );
+}
