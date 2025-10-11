1422fe5ef5ddf6d02978c87f2d01f18cdb83c214
WOLFPACK <officialdavid890@gmail.com>
2025-10-11 07:50:38 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,16 @@
 // pages/_app.js
-
-import "../styles/global.css";
+import "@/styles/global.css";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <AnimatePresence mode="wait" initial={false}>
+    <AnimatePresence mode="wait">
       <motion.div
         key={router.route}
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{
-          duration: 0.6,
-          ease: [0.25, 0.1, 0.25, 1.0],
-        }}
-        className="min-h-screen bg-black text-gray-100"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.6 }}
       >
         <Component {...pageProps} />
       </motion.div>
