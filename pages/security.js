e08d28dd0678c338b5f88879b8f8b6c4ffaa45c0
WOLFPACK <officialdavid890@gmail.com>
2025-10-11 12:48:05 -0700

    Create security.js

---
+++ pages/security.js
@@ -0,0 +1,5 @@
+// pages/how-it-works.js
+import Layout from "../components/Layout";
+export default function HowItWorks() {
+  return <Layout><h1 className="text-3xl font-bold text-gold-400">How It Works</h1><p className="text-gold-300/80 mt-4">Detailed flowcharts and explanations coming soon.</p></Layout>;
+}
