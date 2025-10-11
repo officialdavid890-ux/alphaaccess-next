b25ad81d0d9aba052cb3fc368074b0736a8af16a
WOLFPACK <officialdavid890@gmail.com>
2025-10-10 21:43:09 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "'@/styles/globals.css";
+import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <AnimatePresence mode="wait">
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
