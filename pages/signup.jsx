119e301e8208d0698e8dda2f9ffa6fff0faec859
WOLFPACK <officialdavid890@gmail.com>
2025-10-13 17:15:52 -0700

    Create signup.jsx

---
+++ pages/signup.jsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Create your account</h1>
+        <p className="text-yellow-200/80 mt-2">Signup flow coming soon.</p>
+      </div>
+    </main>
+  );
+}
